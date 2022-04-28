f69dd5cdf0ca60ddf2ecb6adc66515474f9e1c16
Juanki0396 <juanki0396@gmail.com>
2022-04-28 19:22:20 +0200

    added loss plotting

--- src/models/trainning.py
+++ src/models/trainning.py
@@ -2,9 +2,12 @@
 import abc
 from typing import Callable
 
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
+
 
 from src.utils import run_time
 from src.models.base_model import BaseModel, CycleGan, ImageClassifier
@@ -68,13 +71,16 @@ class CycleGanTrainer(BaseTrainer):
 
 class ClassifierTrainer(BaseTrainer):
 
-    def __init__(self, model: ImageClassifier) -> None:
+    def __init__(self, model: ImageClassifier, save_dir: str) -> None:
         super().__init__()
 
         self.model = model
+        self.save_dir = save_dir
         self.epoch_train_loss = []
         self.epoch_validation_loss = []
         self.epoch_metric = []
+        self.metric_model_saved = 0.
+        self.train_loss_model_saved = None
 
     @run_time
     def train_epoch(self):
@@ -113,3 +119,32 @@ class ClassifierTrainer(BaseTrainer):
         mean_metric = np.mean(batch_metrics)
         self.epoch_validation_loss.append(mean_loss)
         self.epoch_metric.append(mean_metric)
+
+    def save_model(self):
+
+        if self.epoch_metric[-1] > self.metric_model_saved:
+            self.model.save_model(self.save_dir)
+            self.metric_model_saved = self.epoch_metric[-1]
+            self.train_loss_model_saved = self.epoch_train_loss[-1]
+            print(f"Best metric achieved: {self.metric_model_saved} -> Model saved")
+
+    def plot_losses(self):
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+        epochs = range(len(self.epoch_train_loss))
+
+        ax1.plot(epochs, self.epoch_train_loss, label="Train")
+        ax1.plot(epochs, self.epoch_validation_loss, label="Validation")
+        ax1.set_ylim(0, 2)
+        ax1.set_xlabel("Epochs")
+        ax1.set_ylabel("BCELoss")
+        ax1.set_title("Losses")
+        ax1.legend()
+
+        ax2.plot(epochs, self.epoch_metric)
+        ax2.set_ylim(0, 1)
+        ax2.set_xlabel("Epochs")
+        ax2.set_ylabel("Accuracy")
+        ax2.set_title("Metrics")
+
+        return fig
