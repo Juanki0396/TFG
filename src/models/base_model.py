6898d8047806efb6da59d584401445eb0048c877
Juanki0396 <juanki0396@gmail.com>
2022-05-22 15:14:02 +0200

    Documentation added and some functions are refactored

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -9,7 +9,8 @@ import torch
 class BaseModel(ABC):
 
     def __init__(self):
-
+        """Instanciate the networks, criterions and optimizers.
+        """
         self.name: str = None
         self.models: Dict[str, torch.nn.Module] = {}
         self.criterions: Dict[str, Callable] = {}
@@ -18,35 +19,73 @@ class BaseModel(ABC):
 
     @abstractmethod
     def set_input(self, input: Dict[str, torch.Tensor]) -> None:
-        pass
+        """This method will store the inputs of the model needed for the 
+        forward method
 
-    @abstractmethod
-    def load_model(self, model_dir: str) -> None:
-        pass
-
-    @abstractmethod
-    def save_model(self, save_dir: str) -> None:
+        Args:
+            input (Dict[str, torch.Tensor])
+        """
         pass
 
     @abstractmethod
     def forward(self) -> None:
+        """Apply the forward step over the stored inputs and store the results.
+        """
         pass
 
     @abstractmethod
     def update_parameters(self) -> Dict[str, float]:
-        pass
+        """Use the forward method and apply the backward step, updating the model weights.
+        It also return in a dictionary the different losses obatined.
+
+        Returns:
+            Dict[str, float]: loss name with its value.
+        """
+        self.set_train_mode(True)
+        self.forward()
+        # Continue the implementation
 
     @abstractmethod
-    def test(self) -> Dict[str, Any]:
+    def validation(self) -> Dict[str, Any]:
+        """Apply the forward step and evaluate the losses and metrics over a validation example
+
+        Returns:
+            Dict[str, Any]: _description_
+        """
+        self.set_train_mode(False)
+        self.forward()
+        # Continue the implementation
+
+    @abstractmethod
+    def inference(self, input: Dict[str, torch.Tensor] = None) -> Dict[str, Any]:
+        """Implement the inference of the model. Input is set to None since generative model don't
+        need any input.
+
+        Args:
+            input (Dict[str, torch.Tensor], optional): Defaults to None.
+
+        Returns:
+            Dict[str, Any]: Model outputs
+        """
         pass
 
     def set_requires_grad(self, nets: List[torch.nn.Module], requires_grad=True) -> None:
+        """Activate or deactivate the gradient computation of the selected nets.
+
+        Args:
+            nets (List[torch.nn.Module]): 
+            requires_grad (bool, optional): Defaults to True.
+        """
         for net in nets:
             for par in net.parameters():
                 par.requires_grad = requires_grad
 
     def set_train_mode(self, train_mode: bool) -> None:
+        """Change the mode of the model between train or eval.
 
+        Args:
+            train_mode (bool): True -> train; False -> eval
+        """
         if train_mode:
             for model in self.models.values():
                 model.train()
@@ -55,6 +94,13 @@ class BaseModel(ABC):
                 model.eval()
 
     def save_model(self, save_dir: str) -> None:
+        """Creates or overwrite a save model folder, where the state dict is saved,
+        in the selected saviing direrctory.
+
+
+        Args:
+            save_dir (str): Directory where the model folder is created.
+        """
 
         model_dir = os.path.join(save_dir, self.name)
         if not os.path.exists(model_dir):
@@ -65,6 +111,11 @@ class BaseModel(ABC):
             torch.save(model.state_dict(), path)
 
     def load_model(self, model_dir: str) -> None:
+        """Load model state dicts stored in the model directory
+
+        Args:
+            model_dir (str): Directory that contains the state dicts of the networks.
+        """
 
         for file in os.scandir(model_dir):
             net_name = file.name.split(".")[0]
